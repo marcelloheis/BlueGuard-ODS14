1ca53de27d9216ef9b3bbee77ad5f8191e181aa6
Marcelo Heis <138878577+marcelloheis@users.noreply.github.com>
2025-10-19 16:38:39 -0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,1 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// Configura proxy para o backend local
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': 'http://localhost:4000'
+    }
+  }
+})
